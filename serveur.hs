5b140005d3ef81dc3162ae5f5f140f8c7f0eb7cc
Thomas Blangero <chungaishaeve@gmail.com>
2018-05-23 10:29:17 +0200

    server ok?

--- serveur.hs
+++ serveur.hs
@@ -8,6 +8,8 @@ main = do
     case a of
       ["slave",file1,file2,file3,port] -> slave file1 file2 file3 (read port :: PortNumber)
 
+-- protocole
+slave :: IOText() -> IOText() -> IOText() -> PortNumber -> IO()
 slave file1 file2 file3 port = withSocketsDo $ do
   g <- readFile file1
   i <- readFile file2
@@ -15,14 +17,16 @@ slave file1 file2 file3 port = withSocketsDo $ do
   sock <- listenOn $ PortNumber port
   slavebody sock g i s
 
+-- action serveur
+slavebody :: Socket -> IOText() -> IOText() -> IOText() -> IO()
 slavebody sock g i s =
  forever $ do
-  (handle,host,port) <- accept sock
-  rc <- hGetLine handle
+  -- (handle,host,port) <- accept sock
+  -- rc <- hGetLine handle
   let graph = read g :: [Relation]
        info = read i :: [Infos]
        st = read s :: [Style]
        sujets = getSujets graph
        nomFichier = map (++".html") sujets
        contenu = map (\subject -> creerPage subject graph infoGraph styleGraph) sujets
-  hPutStrLn handle sequence(zipWith writeFile nomFichier contenu)
+  send sock sequence(zipWith writeFile nomFichier contenu)
